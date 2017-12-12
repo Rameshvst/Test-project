9645227dbc6ad89a3b43b7f329bfd68b810cc802
Ramesh-HP\Ramesh <ramesh.vst@gmail.com>
2017-12-12 11:15:48 -0500

    commit the python matlap code

---
+++ matlap.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 21:10:39 2017
+
+@author: Ramesh
+"""
+
+import matplotlib.pyplot as plt
+
+#️Creation of axis
+x=[1,2,3,4]
+y=[1,2,3,4]
+y2=[1,4,9,16]
+y3=[1,8,27,64]
+
+#️
